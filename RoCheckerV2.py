bfca8fbb2682016eebef62c059d754f1f9cb391e
Azure <leonpugh123@live.co.uk>
2018-10-14 12:01:05 +0100

    Update RoCheckerV2.py

--- RoCheckerV2.py
+++ RoCheckerV2.py
@@ -3,14 +3,13 @@ from requests_html import HTMLSession
 
 print("Welcome to the new and improved RoChecker V2!\nHere are our updates:\n1. Ban filter (Find Available BUT banned names)\n2. Name size is configurable!\nIf you enjoy this script, or have any issues, contact the owner on Discord! (Azure#0263)\nCredit to 'Change Name' for the ban filter idea!\n\nTo begin, enter the amount of letters you want your name to be! (Minimum and Maximum)!")
 
-MinAmount = int(input("Minimum Amount of Characters 3 - ???: "))
 MaxAmount = int(input("Maximum Amount of Characters ??? - 20: "))
 
 chars = string.ascii_letters + string.digits
 session = HTMLSession()
 
 while True:
-    name = ''.join(random.choice(chars) for i in range(MinAmount, MaxAmount))
+    name = ''.join(random.choice(chars) for i in range(0, MaxAmount))
     r = session.get(f'https://www.roblox.com/UserCheck/doesusernameexist?username={name}')
     if r.html.search('True'):
         print(name, "[TAKEN]")
@@ -23,4 +22,4 @@ while True:
           f = open('names.txt', 'a+')
           f.write(name + "[AVAILABLE]\n")
           f.close()
-          time.sleep(0.1)
+        time.sleep(0.1)
