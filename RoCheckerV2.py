8ef4f4b5c5dfe14a899db6cb1cd39ef97fdabb40
Azure <leonpugh123@live.co.uk>
2018-10-17 16:45:54 +0100

    Update RoCheckerV2.py

--- RoCheckerV2.py
+++ RoCheckerV2.py
@@ -1,25 +1,30 @@
-import string, random, time
+import string, random, time, threading
 from requests_html import HTMLSession
 
-print("Welcome to the new and improved RoChecker V2!\nHere are our updates:\n1. Ban filter (Find Available BUT banned names)\n2. Name size is configurable!\nIf you enjoy this script, or have any issues, contact the owner on Discord! (Azure#0263)\nCredit to 'Change Name' for the ban filter idea!\n\nTo begin, enter the amount of letters you want your name to be! (Minimum and Maximum)!")
+print("Welcome to the new and improved RoChecker V2!\nHere are our updates:\n1. Ban filter (Find Available BUT banned names)\n2. Name size is configurable!\nIf you enjoy this script, or have any issues, contact the owner on Discord! (Azure#0263)\nCredit to 'Change Name' for the ban filter idea!\n\nTo begin, enter the amount of letters you want your name to be! (Maximum)!")
 
 MaxAmount = int(input("Maximum Amount of Characters ??? - 20: "))
 
 chars = string.ascii_letters + string.digits
 session = HTMLSession()
 
-while True:
+
+def search():
+  while True:
+    time.sleep(0.1)
     name = ''.join(random.choice(chars) for i in range(0, MaxAmount))
     r = session.get(f'https://www.roblox.com/UserCheck/doesusernameexist?username={name}')
-    if r.html.search('True'):
-        print(name, "[TAKEN]")
-    else:
+    if r.html.search('true'):
+      print(f"{name} [TAKEN]!")
+
+    if r.html.search('false'):
+      try:
         r2 = session.get(f'https://www.roblox.com/User.aspx?Username={name}', allow_redirects=True)
-        if r2.html.search("404"):
-          print(name, "[BANNED]")
+        
+        if r2.html.search('404'):
+          print(f"{name} [BANNED]")
         else:
-          print(f"{name} [AVAILABLE]!")
-          f = open('names.txt', 'a+')
-          f.write(name + "[AVAILABLE]\n")
-          f.close()
-        time.sleep(0.1)
+          print(f"{name} [AVAILABLE]")
+      except:
+        break
+search()
